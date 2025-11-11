6e1799507586912f16776a6d42d573fb622d3f98
kawacukennedy <kawacukent@gmail.com>
2025-11-11 10:53:59 +0200

    Add React import to fix JSX parsing

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import { useInView } from "react-intersection-observer";
 import ProgressBar from "../components/ProgressBar";
